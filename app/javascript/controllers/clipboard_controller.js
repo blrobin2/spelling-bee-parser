fa79f513cfa45394fe5fd0ded15fb6b17f981829
Bennie Robinson <brobinson88@gmail.com>
2024-01-23 18:18:06 -0500

    Make copy button nicer

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,11 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static targets = ["source"]
-  connect() {
-  }
+  static targets = ["source", "button"]
 
   copy() {
-    navigator.clipboard.writeText(this.sourceTarget.innerText)
+    navigator.clipboard.writeText(this.sourceText)
+    this.buttonTarget.textContent = "Copied!"
+    this.buttonTarget.classList.add("disabled")
+  }
+
+  get sourceText() {
+    return this.sourceTarget.innerText
   }
 }
