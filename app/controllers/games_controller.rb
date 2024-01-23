a9926948295c6913db34104a5b8ed0884a5924e1
Bennie Robinson <brobinson88@gmail.com>
2024-01-23 12:50:20 -0500

    Run through rubocop

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,12 @@
 class GamesController < ApplicationController
   def index
     @url = game_params[:url]
-    if @url.present? && @url =~ URI::regexp
+    if @url.present? && @url =~ URI::DEFAULT_PARSER.make_regexp
       @uri = URI.parse(@url)
       @hash = Rack::Utils.parse_nested_query(@uri&.query)
 
       unless @hash.empty?
-        @game_date = Date.parse(@hash['d'])&.to_formatted_s(:long)
+        @game_date = Date.parse(@hash['d'])&.to_fs(:long)
         @game_rank = @hash['r']
         @points = @hash['p']
       end
@@ -19,7 +19,7 @@ class GamesController < ApplicationController
 
   private
 
-  def game_params
-    params.permit(:url, :commit)
-  end
+    def game_params
+      params.permit(:url, :commit)
+    end
 end
